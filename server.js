6591490bf8cdaa3aae61f782a4cdb3233ac98b59
s6stuc <s.craigen@mac.com>
2019-08-06 15:32:57 +0100

    Debugging deployment issues

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const port = process.env.PORT || 5000
 const api = require('./api/api')
+const fs = require('fs')
 const connectionString = process.env.S107D01_MONGO_01 || process.env.MONGO //.replace(/\/s107d01-mongo-01\?/, '/testing?')
 const config = {
   // dataSource: require('./api/adaptors/mongo/mongoAdaptor')({ connectionString: process.env.S107D01_MONGO_01 })
@@ -11,10 +12,16 @@ const config = {
 const app = api(config)
 
 const serveStatic = require('serve-static')
-app.use(serveStatic('build/', { index: ['index.html'] }))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build/index.html'))
-})
+
+if (fs.existsSync(path.join(__dirname, 'build/index.html'))) {
+  console.log('exists')
+  app.use(serveStatic('build/', { index: ['index.html'] }))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/index.html'))
+  })
+} else {
+  app.get('*', (req, res) => res.send('NOT FOUND'))
+}
 
 const server = app.listen(port, () => {
   console.log('Magic happens on port ' + port)
