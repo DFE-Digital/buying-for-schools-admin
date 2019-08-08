f911a3e3123d3fafb318896606d0b92866b45370
s6stuc <s.craigen@mac.com>
2019-08-08 15:20:15 +0100

    dockertest

--- server.js
+++ server.js
@@ -3,26 +3,35 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const serveStatic = require('serve-static')
 const path = require('path')
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8000
 
 const app = express()
 app.use(bodyParser.json())
 
-const api = require('./api/api')
-const config = {
-  dataSource: require('./api/adaptors/mongodoc/mongodocAdaptor')({ connectionString: process.env.MONGO })
-}
-api(app, config)
+const haveConnectionDetails = !!process.env.MONGO
+const haveBuildDirectory = fs.existsSync(path.join(__dirname, 'build/index.html'))
 
-if (fs.existsSync(path.join(__dirname, 'build/index.html'))) {
+
+if (haveConnectionDetails) {
+  const api = require('./api/api')
+  const config = {
+    dataSource: require('./api/adaptors/mongodoc/mongodocAdaptor')({ connectionString: process.env.MONGO })
+  }
+  api(app, config)
+} else {
+  app.get('/api/*', (req, res) => res.send({ haveConnectionDetails }))
+}
+
+if (haveBuildDirectory) {
   app.use(serveStatic('build/', { index: ['index.html'] }))
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build/index.html'))
   })
 } else {
-  app.get('*', (req, res) => res.send('NOT FOUND'))
+  app.get('*', (req, res) => res.send({ haveConnectionDetails, haveBuildDirectory, port }))
 }
 
 const server = app.listen(port, () => {
   console.log('Magic happens on port ' + port)
+  console.log({ haveConnectionDetails, haveBuildDirectory, port })
 })
