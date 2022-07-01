d47c0e0caaa181c187d4792669e4960ea4d4a312
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2022-07-01 09:10:38 +0100

    Add webhookService and tests

---
+++ api/services/webhookService.js
@@ -0,0 +1,27 @@
+const send = async (payload) => {
+  const https = require('https')
+
+  const data = JSON.stringify(payload)
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(data),
+      'Authorization': `Bearer ${process.env.GHBS_WEBHOOK_SECRET}`
+    }
+  }
+  
+  const req = https.request(process.env.GHBS_WEBHOOK_ENDPOINT, options, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+  })
+
+  req.on('error', error => {
+    console.error(error)
+  })
+
+  req.write(data)
+  req.end()
+}
+
+module.exports = { send }
